cf8287f307a7a0e521bc33e272591a917dee2922
Drew DeVault <sir@cmpwn.com>
2013-11-05 19:43:37 -0700

    Initial commit

---
+++ MediaCrush/Program.cs
@@ -0,0 +1,57 @@
+﻿using MediaCrush;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaCrushWindows
+{
+    static class Program
+    {
+        static UploadWindow UploadWindow;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var icon = new NotifyIcon();
+            icon.Icon = Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly().Location);
+            icon.Visible = true;
+            icon.Text = "Click here to upload files to MediaCrush";
+
+            var menu = new ContextMenu();
+            var settings = new MenuItem("Settings");
+            var exit = new MenuItem("Exit");
+            menu.MenuItems.Add(settings);
+            menu.MenuItems.Add(exit);
+            exit.Click += (s, e) => Application.Exit();
+            settings.Click += settings_Click;
+            icon.ContextMenu = menu;
+
+            icon.Click += icon_Click;
+
+            Application.ApplicationExit += (s, e) => icon.Dispose();
+            Application.Run();
+        }
+
+        static void icon_Click(object sender, EventArgs e)
+        {
+            if (UploadWindow != null)
+                return;
+            UploadWindow = new UploadWindow();
+            UploadWindow.Closed += (s, f) => UploadWindow = null;
+            UploadWindow.Show();
+        }
+
+        static void settings_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MediaCrush/UploadWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MediaCrush
+{
+    /// <summary>
+    /// Interaction logic for UploadWindow.xaml
+    /// </summary>
+    public partial class UploadWindow : Window
+    {
+        public ObservableCollection<UploadingFile> FileList;
+
+        public UploadWindow()
+        {
+            InitializeComponent();
+            FileList = new ObservableCollection<UploadingFile>();
+            Left = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right - this.Width;
+            Top = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Bottom - this.Height;
+            AllowDrop = true;
+            DragEnter += UploadWindow_DragEnter;
+            Drop += UploadWindow_Drop;
+        }
+
+        void UploadWindow_Drop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files)
+                UploadFile(file);
+        }
+
+        void UploadWindow_DragEnter(object sender, System.Windows.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = System.Windows.DragDropEffects.Copy;
+            else
+                e.Effects = System.Windows.DragDropEffects.None;
+        }
+
+        private void DismissButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UploadFile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.Filter = "Media files|*.png;*.jpg;*.jpe;*.jpeg;*.gif;*.svg;*.mp4;*.ogg;*.oga;*.ogv;*.webm;*.mp3";
+            dialog.Multiselect = true;
+            var result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                // Upload files
+                foreach (var file in dialog.FileNames)
+                    UploadFile(file);
+            }
+        }
+
+        private void UploadFile(string file)
+        {
+            // Modify UI
+            if (FileList.Count == 0)
+            {
+                rowCollapse1.Height = new GridLength(0);
+                rowCollapse2.Height = new GridLength(0);
+                rowExpand.Height = new GridLength(5, GridUnitType.Star);
+                uploadingFilesContainer.Visibility = Visibility.Visible;
+                uploadingFiles.ItemsSource = FileList;
+            }
+            FileList.Add(new UploadingFile(file));
+        }
+    }
+}
